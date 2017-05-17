f0409251c5b95783b530a199c608ad22b2475721
Pandaaaa906 <ye.pandaaaa906@gmail.com>
2017-05-18 01:52:07 +0800

    Signed-off-by: Pandaaaa906 <ye.pandaaaa906@gmail.com>

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import urllib2
 import json
 from google.appengine.api import memcache
-
-from flask import Flask, jsonify
+from flask import Flask, jsonify, session
 from flask import request
 from functools import wraps
 from functions.WXBizDataCrypt import WXBizDataCrypt
@@ -10,6 +9,7 @@ from functions.WXBizDataCrypt import WXBizDataCrypt
 app = Flask(__name__)
 app.config['app_id'] = "wxc2794b66adfdb911"
 app.config['app_secret'] = "cbf3448395b6129dd4b08ed3b057ad7d"
+app.config['SECRET_KEY'] = '\x14\x02\xacQ{\x17I\xbbU\xe1\xe1\xf2`\x05\x0fS\xd4\x0eC\x8d)\xa4\xe6\xb6'
 
 
 def check_wx_referer(f):
@@ -30,18 +30,24 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/wx/login')
+@app.route('/wx/login', methods=["GET", "POST"])
 @check_wx_referer
 def login():
     app_id = app.config.get('app_id', None)
-    sessionKey = 'tiihtNczf5v6AKRyjwEUhQ=='
-    encryptedData = 'CiyLU1Aw2KjvrjMdj8YKliAjtP4gsMZMQmRzooG2xrDcvSnxIMXFufNstNGTyaGS9uT5geRa0W4oTOb1WT7fJlAC+oNPdbB+3hVbJSRgv+4lGOETKUQz6OYStslQ142dNCuabNPGBzlooOmB231qMM85d2/fV6ChevvXvQP8Hkue1poOFtnEtpyxVLW1zAo6/1Xx1COxFvrc2d7UL/lmHInNlxuacJXwu0fjpXfz/YqYzBIBzD6WUfTIF9GRHpOn/Hz7saL8xz+W//FRAUid1OksQaQx4CMs8LOddcQhULW4ucetDf96JcR3g0gfRK4PC7E/r7Z6xNrXd2UIeorGj5Ef7b1pJAYB6Y5anaHqZ9J6nKEBvB4DnNLIVWSgARns/8wR2SiRS7MNACwTyrGvt9ts8p12PKFdlqYTopNHR1Vf7XjfhQlVsAJdNiKdYmYVoKlaRv85IfVunYzO0IKXsyl7JCUjCpoG20f0a04COwfneQAGGwd5oa+T8yO5hzuyDb/XcxxmK01EpqOyuxINew=='
-    iv = 'r7BXXKkLb8qrSNn05n0qiA=='
-    return
+    j_request = request.get_json()
+    # userInfo = j_request.get("userInfo")
+    open_id = request.args.get("open_id")
+    session_key = memcache.get(open_id)
+    encryptedData = j_request.get('encryptedData')
+    iv = j_request.get('iv')
+    pc = WXBizDataCrypt(app_id, session_key)
+    obj = pc.decrypt(encryptedData, iv)
+    return jsonify(obj)
+    # TODO check&add user to database
 
 
 @app.route('/wx/get_session_key')
-#@check_wx_referer
+# @check_wx_referer
 def get_session_key():
     js_code = request.args.get('js_code', '')
     app_id = app.config.get('app_id', None)
@@ -52,14 +58,13 @@ def get_session_key():
     errcode = j_obj.get("errcode", None)
     if errcode:
         msg = j_obj.get("errmsg", None)
-        return jsonify(status="fail", errcode="errcode", msg=msg)
+        return jsonify(status="fail", errcode=errcode, msg=msg)
     else:
-        open_id = j_obj.get("open_id", None)
-        session_key = j_obj.get("session_key", None)
+        open_id = j_obj.get("openid", "None")
+        session_key = j_obj.get("session_key", "None")
         expires_in = j_obj.get("expires_in", 1500)
-        memcache.add(open_id, session_key, expires_in)
-        return jsonify(status="success")
-
+        memcache.set(open_id, session_key, expires_in)
+        return jsonify(status="success", open_id=open_id)
 
 
 """
@@ -69,5 +74,6 @@ def rout_handler(app, ctrl, pk):
 """
 
 if __name__ == '__main__':
-    app.debug(True)
-    app.run()
+    session.permanent = True
+    app.secret_key = '\x14\x02\xacQ{\x17I\xbbU\xe1\xe1\xf2`\x05\x0fS\xd4\x0eC\x8d)\xa4\xe6\xb6'
+    app.run(debug=True)
